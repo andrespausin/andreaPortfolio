c2a50582a0a025bfd433dd7fe300a351d88428a1
andrespausin <candrespausin2001@gmail.com>
2025-01-26 15:19:17 -0400

    add: banner and its values

---
+++ src/components/body/banner/bannerValues.js
@@ -0,0 +1,4 @@
+export const bannerValues = {
+    title: "Andrea Chacin",
+    text: "Holaa soy Andrea Chacin, arquitecto graduada de la universidad José Antonio Páez en mi país Venezuela. En mi profesión, he tenido la oportunidad de desarrollar diversos proyectos que me han permitido crecer tanto a nivel profesional como personal. Me siento orgullosa de los logros alcanzados hasta ahora, pero siempre estoy buscando nuevos desafíos. La neuroarquitectura me ha abierto un mundo de posibilidades para diseñar espacios que vayan más allá de lo funcional y estético. Mi visión es crear ambientes que no solo sean agradables a la vista, sino que también tengan un impacto positivo en el bienestar de quienes los habitan. Aspiro a seguir aprendiendo y creciendo en este campo, dejando mi huella en el mundo a través de mis diseños."
+}
