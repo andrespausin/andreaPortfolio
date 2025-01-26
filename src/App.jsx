697f6911f759ae22ec3792b9add06c1083913598
andrespausin <candrespausin2001@gmail.com>
2025-01-26 15:19:49 -0400

    app modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { Header } from './components/Header/heaeder.jsx'
+import { Banner } from './components/body/banner/banner.jsx'
+import { Footer } from './components/footer/footer.jsx'
+import './App.css'
+import {bannerValues} from './components/body/banner/bannerValues.js'
 
 function App() {
   return (
     <>
       <Header />
-      <p>Hola mundo</p>
+      <Banner text={bannerValues.text} title={bannerValues.title} />
+      <Footer />
     </>
   )
 }
