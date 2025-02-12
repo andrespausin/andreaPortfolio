fcfac334809dde65a11fd0f0e2cbafb69b0f753f
andrespausin <candrespausin2001@gmail.com>
2025-02-12 19:40:49 -0400

    delete: delete unuseful components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { Header } from './components/Header/heaeder.jsx'
-import { Banner } from './components/body/banner/banner.jsx'
-import { Footer } from './components/footer/footer.jsx'
 import './App.css'
-import {bannerValues} from './components/body/banner/bannerValues.js'
 
 function App() {
   return (
     <>
-      <Header />
-      <Banner text={bannerValues.text} title={bannerValues.title} />
-      <Footer />
+      <h1>React App</h1>
     </>
   )
 }
